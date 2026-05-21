d658a8dcd9e5b35a7ebd51c1b17161f43365d860
jennguyen-1 <nguyenjp7@vcu.edu>
2026-05-21 14:21:35 -0400

    portfolio2

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+$(document).ready(function() {
+$('#container').mousewheel(function(e, delta) {
+    if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+      this.scrollLeft -= (delta);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+      e.preventDefault();
+    }
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
+});
+});
